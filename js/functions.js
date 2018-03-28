51919940d9f9ebebbc5888b9e6c0be132236e664
Addison Scott-Fleming <addisons@student.unimelb.edu.au>
2018-03-29 00:24:42 +1100

    no message

--- js/functions.js
+++ js/functions.js
@@ -2,15 +2,30 @@ $(document).ready(function() {
     // $("#whatsComing").hide();
 
     const comingHeight = $("#whatsComing").css('height');
+    const interestHeight = $("#expressInterest").css('height');
+
     $("#whatsComing").animate({height: '0', opacity: '0'}, 0);
+    $("#expressInterest").animate({height: '0', opacity: '0'}, 0);
 
     $("#showComing").on("click", function() {
-       if ($("#whatsComing").css("opacity") == '1') {
-           $("#whatsComing").animate({height: '0', opacity: '0'}, 300);
-       }
+        if ($("#whatsComing").css("opacity") == '1') {
+            $("#whatsComing").animate({height: '0', opacity: '0'}, 300);
+        }
 
-       else {
-           $("#whatsComing").animate({height: comingHeight, opacity: '1'}, 300);
-       }
-   });
+        else {
+            $("#whatsComing").animate({height: comingHeight, opacity: '1'}, 300);
+            $("#expressInterest").animate({height: '0', opacity: '0'}, 300);
+        }
+    });
+
+    $("#showInterest").on("click", function() {
+        if ($("#expressInterest").css("opacity") == '1') {
+            $("#expressInterest").animate({height: '0', opacity: '0'}, 300);
+        }
+
+        else {
+            $("#expressInterest").animate({height: interestHeight, opacity: '1'}, 300);
+            $("#whatsComing").animate({height: '0', opacity: '0'}, 300);
+        }
+    });
 });
