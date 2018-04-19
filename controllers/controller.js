15d7c863642f09383ed8a2d2de97eeea0d9e8c15
Addison Scott-Fleming <addisons@student.unimelb.edu.au>
2018-04-19 20:15:23 +1000

    no message

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,11 +1,7 @@
-const fetchLanding = (req, res) => {
-    res.render("landing");
-};
-
 const fetchHome = (req, res) => {
     res.render("home");
 };
 
 module.exports = {
-    fetchLanding, fetchHome
+    fetchHome
 };
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/controller");
 
-router.get("/", controller.fetchLanding);
-router.get("/home", controller.fetchHome);
+router.get("/", controller.fetchHome);
 
 module.exports = router;
