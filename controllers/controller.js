11318f7afd13cbac9f1c07cbbb96fefb9f71a6d0
wmaurora <willm@wth.com.au>
2018-04-21 10:26:55 +1000

    finished hardcoded version

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,6 +2,12 @@ const fetchHome = (req, res) => {
     res.render("home");
 };
 
+const fetchHomePage= (req, res) => {
+    res.render("homePage");
+};
+
+
+
 module.exports = {
-    fetchHome
+    fetchHome, fetchHomePage
 };
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/controller");
 
-router.get("/", controller.fetchHome);
+router.get("/home", controller.fetchHome);
+router.get("/HomePage", controller.fetchHomePage);
 
 module.exports = router;
