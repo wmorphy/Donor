43319aea75f692661685367fc6103baa33a5370e
dunnej1 <dunnej1@student.unimelb.edu.au>
2018-04-21 15:46:52 +1000

    added landing page, account creation page, appointment booking page.

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,8 +4,20 @@ const fetchHome = (req, res) => {
 
 const fetchProfile = (req, res) => {
     res.render("profile");
-}
+};
+
+const fetchIndex = (req, res) => {
+    res.render("index");
+};
+
+const fetchScheduler = (req, res) => {
+    res.render("scheduler");
+};
+
+const fetchCreateAccount = (req, res) => {
+    res.render("createaccount");
+};
 
 module.exports = {
-    fetchHome, fetchProfile
+    fetchHome, fetchProfile, fetchIndex, fetchScheduler, fetchCreateAccount
 };
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/controller");
 
-router.get("/", controller.fetchHome);
-router.get("/profile", controller.fetchProfile)
+router.get("/", controller.fetchIndex);
+router.get("/home", controller.fetchHome);
+router.get("/profile", controller.fetchProfile);
+router.get("/scheduler", controller.fetchScheduler);
+router.get("/createaccount", controller.fetchCreateAccount);
 
 module.exports = router;
