1cb2e018d1a7413eb0d6134c6ea28d3719149218
wmaurora <willm@wth.com.au>
2018-05-28 07:37:37 +1000

    Added teams you might like

--- js/teams_data.js
+++ js/teams_data.js
@@ -40,9 +40,35 @@ function processTeamData() {
                 }
 
             })
+            $.each(data, function(){
+                if (this.email != userid) {
+                    //var html = '<a href="/teamsprofile/'+this.link+'/'+ this.teamname +'/'+ this.members +/'" class="cardLink text-body mx-1 mt-3">'
+                    // var html = '<a href="" class="cardLink text-body mx-1 mt-3">'
+                    html = '<div class="card px-3 pt-3 pb-2" style="width:175px;">'
+                    html += '<img class="card-img-top rounded" src="../img/'+ this.link +'" alt="Card image">'
+                    html += '<hr class="mb-2"/>'
+                    html += '<div class="card-body px-2 py-1 ">'
+                    html += '<h5 class="card-title text-center font-rubik my-1"> '+this.teamname+' </h5>'
+                    html += '<p class="mb-0 text-center"><img src="img/torso.png" width="16px" class="mr-1">'+this.members.length+'</p>'
+                    // html += '</div></div></a>'
+                    html += "</div></div>"
+
+                    // append the previous donations to the div prevDon
+                    $("#suggestedPage").append(html);
+                }
+
+            })
         });
+
 
         $(".loaderDiv").hide();
         $(".contentDiv").animate({opacity: 1});
     });
 };
+
+
+
+
+
+
+
